0227d50308f12fb2ddc28d396ced22b844e97fd5
Riany Mello <mello.riany@gmail.com>
2024-01-04 10:22:23 +0000

    update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import DotGroup from "./scenes/DotGroup";
 import MySkills from "./scenes/MySkills";
 import LineGradient from "./components/LineGradient";
 import Projects from "./scenes/Projects";
-import Contact from "./scenes/Contact";
 import Footer from "./scenes/Footer";
 import useMediaQuery from "./hooks/useMediaQuery";
 import { useEffect, useState } from "react";
-import Testimonials from "./scenes/Testimonials";
 import { motion } from "framer-motion";
 
 function App() {
@@ -60,7 +58,7 @@ function App() {
           <MySkills />
         </motion.div>
       </div>
-      <LineGradient />
+     
       <div className="w-5/6 mx-auto">
         <motion.div
           margin="0 0 -200px 0"
@@ -70,26 +68,7 @@ function App() {
           <Projects />
         </motion.div>
       </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("testimonials")}
-        >
-          <Testimonials />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("contact")}
-        >
-          <Contact />
-        </motion.div>
-      </div>
+      
       <Footer />
     </div>
   );
